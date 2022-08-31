ed83025e43b5b3798fd6066940b2a28fef67f5f8
beanb2 <brennan.bean@usu.edu>
2022-08-31 09:49:08 -0600

    first version of package list

---
+++ essential_r_packages.R
@@ -0,0 +1,27 @@
+# 1. (ALL) For manipulating data.
+install.packages(c("tidyverse", "data.table"))
+
+# 2. (ALL) For developing my own R packages and installing non-CRAN packages
+install.packages(c("devtools", "usethis"))
+
+# 3. (ALL) For drafting reports and programming style
+install.packages(c("rmarkdown", "knitr", "lintr", "styler"))
+
+# 5. (ADVANCED) For advanced programming in R
+install.packages(c("Rcpp", "RcppArmadillo"))
+
+# 6. (ADVANCED) For extreme value estimation and non-linear optimization
+install.packages(c("extRemes", "evd", "nloptr", "fitdistrplus"))
+
+# 4. (STAT VIS I and II) For interactive plots
+install.packages(c("Rshiny", "plotly"))
+
+# 7. (SPATIAL STATS) For Spatial Data Analysis
+install.packages(c("sf", "sp", "stars", "raster", "terra", "ncdf4"))
+
+# 7. (OTHER) For some standard machine learning and regression models
+install.packages(c("randomForest", "ranger", "rpart", "mgcv"))
+
+
+
+
